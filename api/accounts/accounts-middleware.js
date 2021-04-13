292a2f1811c624e6e8a99cedc986b4fb75fa81c4
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-12 21:01:06 -0400

    checkAccountId middleware up

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,32 +1,47 @@
+const Accounts = require('./accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
-  const {name, budget} = req.body;
-  try{
-    if(!name || !budget) {
-      next({message: 'name and budget are required', status: 400})
-    } else if(typeof name !== 'string') {
-      next({message: 'name of account must be a string', status: 400})
-    } else if (name.trim().length < 3 || name.trim().length > 100) {
-      next({message: 'name of account must be between 3 and 100', status: 400})
-    } else if(typeof budget !== 'number') {
-      next({message: 'must be a number', status: 400})
-    } else if (budget < 0 || budget > 1000000){
-      next({message:'budget of account is too large or too small',  status: 400})
-    } else {
-      req.account = req.body;
-      next()
-    }
-  }catch(err) {
+	const { name, budget } = req.body;
+	try {
+		if (!name || !budget) {
+			next({ message: 'name and budget are required', status: 400 });
+		} else if (typeof name !== 'string') {
+			next({ message: 'name of account must be a string', status: 400 });
+		} else if (name.trim().length < 3 || name.trim().length > 100) {
+			next({ message: 'name of account must be between 3 and 100', status: 400 });
+		} else if (typeof budget !== 'number') {
+			next({ message: 'must be a number', status: 400 });
+		} else if (budget < 0 || budget > 1000000) {
+			next({ message: 'budget of account is too large or too small', status: 400 });
+		} else {
+			req.account = req.body;
+			next();
+		}
+	} catch (err) {
+		next(err);
+	}
+};
+
+exports.checkAccountNameUnique = async (req, res, next) => {
+	// try{
+	//   if
+	// } catch(err) {
+	//   next(err)
+	// }
+};
+
+exports.checkAccountId = async (req, res, next) => {
+	try {
+		const account = await Accounts.getById(req.params.id);
+		if (account) {
+			next();
+		} else {
+			next({ message: 'account not found', status: 404 });
+		}
+	} catch (err) {
     next(err)
   }
-}
-
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
-
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+};
 
 //eslint-disable-next-line
 exports.errorHandler = (err, req, res, next) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Account = require('./accounts-model');
-const { errorHandler, checkAccountPayload } = require('./accounts-middleware');
+const { errorHandler, checkAccountPayload, checkAccountId } = require('./accounts-middleware');
 
 router.get('/', async (req, res, next) => {
 	try {
@@ -11,7 +11,7 @@ router.get('/', async (req, res, next) => {
 	}
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', checkAccountId ,async (req, res, next) => {
 	try {
 		const account = await Account.getById(req.params.id);
 		res.json(account);
@@ -21,24 +21,28 @@ router.get('/:id', async (req, res, next) => {
 });
 
 router.post('/', checkAccountPayload, async (req, res, next) => {
+  const contents = {
+    name: req.body.name.trim(),
+    budget: req.body.budget
+  }
 	try {
-		const newAccount = await Account.create(req.body);
+		const newAccount = await Account.create(contents);
 		res.status(201).json(newAccount);
 	} catch (err) {
 		next(err);
 	}
 });
 
-router.put('/:id', checkAccountPayload, async (req, res, next) => {
+router.put('/:id', checkAccountId, checkAccountPayload, async (req, res, next) => {
 	try {
 		const updatedAccount = await Account.updateById(req.params.id, req.body);
-		res.status(201).json(updatedAccount);
+		res.json(updatedAccount);
 	} catch (err) {
 		next(err);
 	}
 });
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', checkAccountId, async (req, res, next) => {
   try {
     const deletedAccount = await Account.deleteById(req.params.id);
     res.json(deletedAccount);
